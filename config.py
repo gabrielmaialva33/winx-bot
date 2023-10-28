b638df59318ce55235d10e5d9866ffc990b38d94
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-10-27 21:21:21 -0300

    chore: initial commit

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/gabrielmaialva33/winx-bot.git",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
