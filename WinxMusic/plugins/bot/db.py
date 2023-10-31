5b52919c7220ec08d8ff10a87511a105f1171cb1
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-10-31 11:19:08 -0300

    chore: initial commit

---
+++ WinxMusic/plugins/bot/db.py
@@ -0,0 +1,13 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from WinxMusic import app, LOGGER
+from WinxMusic.utils.database import get_lang, add_served_chat, get_served_chats
+
+
+@app.on_message(filters.group)
+async def monitor_chats(_client: Client, message: Message):
+    served_chats = await get_served_chats()
+    if message.chat.id not in served_chats:
+        await add_served_chat(message.chat.id)
+        LOGGER.info(f"Added {message.chat.id} to served chats.")
