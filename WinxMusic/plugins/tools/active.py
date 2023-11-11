74ae1097732dff8a8bf2aaf42bbd8ee4100c220e
Gabriel Maia <gabrielmaialva33@gmail.com>
2023-11-10 21:17:27 -0300

    refactor: active module

--- WinxMusic/plugins/tools/active.py
+++ WinxMusic/plugins/tools/active.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from WinxMusic import app
+from WinxMusic import app, LOGGER
 from WinxMusic.misc import SUDOERS
 from WinxMusic.utils.database import (
     get_active_chats,
@@ -20,50 +20,42 @@ async def activevc(_, message: Message):
     for x in served_chats:
         try:
             title = (await app.get_chat(x)).title
-            count = (await app.get_chat(x)).members_count
         except:
             await remove_active_chat(x)
             continue
         try:
+            # if chat is private
             if (await app.get_chat(x)).username:
                 user = (await app.get_chat(x)).username
                 count = (await app.get_chat(x)).members_count
                 invite = await app.export_chat_invite_link(x)
                 is_scam = (await app.get_chat(x)).is_scam
                 is_fake = (await app.get_chat(x)).is_fake
-                linked_chat = (await app.get_chat(x)).linked_chat
-                linked_chat_count = (await app.get_chat(linked_chat)).members_count
                 text += (
                     f"<b>{j + 1} ➜ </b> <a href=https://t.me/{user}>{title}</a> [<code>{x}</code>]\n"
                     f"<b>👥 𝗠𝗲𝗺𝗯𝗿𝗼𝘀:</b> <code>{count}</code>\n"
                     f"<b>🚫 𝗘𝘀𝗰𝗮𝗺𝗼𝘀𝗼:</b> {is_scam}\n"
                     f"<b>🚫 𝗙𝗮𝗸𝗲:</b> {is_fake}\n"
-                    f"<b>🔗 𝗟𝗶𝗻𝗸𝗲𝗱 𝗖𝗵𝗮𝘁:</b> {linked_chat.title}\n"
-                    f"<b>👥 𝗠𝗲𝗺𝗯𝗿𝗼𝘀 𝗱𝗼 𝗖𝗵𝗮𝘁 𝗟𝗶𝗻𝗸𝗲𝗮𝗱𝗼:</b> <code>{linked_chat_count}</code>\n"
                     f"<b>🔗 𝗜𝗻𝘃𝗶𝘁𝗲:</b> {invite}\n\n"
                 )
+            # if chat is public
             else:
                 count = (await app.get_chat(x)).members_count
                 invite = await app.export_chat_invite_link(x)
                 is_scam = (await app.get_chat(x)).is_scam
                 is_fake = (await app.get_chat(x)).is_fake
                 linked_chat = (await app.get_chat(x)).linked_chat
-                linked_chat_count = (await app.get_chat(linked_chat)).members_count
                 text += (
                     f"<b>{j + 1} ➜ </b> {title} [<code>{x}</code>]\n"
                     f"<b>👥 𝗠𝗲𝗺𝗯𝗿𝗼𝘀:</b> <code>{count}</code>\n"
                     f"<b>🚫 𝗘𝘀𝗰𝗮𝗺𝗼𝘀𝗼:</b> {is_scam}\n"
                     f"<b>🚫 𝗙𝗮𝗸𝗲:</b> {is_fake}\n"
-                    f"<b>🔗 𝗟𝗶𝗻𝗸𝗲𝗱 𝗖𝗵𝗮𝘁:</b> {linked_chat.title}\n"
-                    f"<b>👥 𝗠𝗲𝗺𝗯𝗿𝗼𝘀 𝗱𝗼 𝗖𝗵𝗮𝘁 𝗟𝗶𝗻𝗸𝗲𝗮𝗱𝗼:</b> <code>{linked_chat_count}</code>\n"
+                    f"<b>🔗 𝗟𝗶𝗻𝗸𝗲𝗱 𝗖𝗵𝗮𝘁:</b> <a href=https://t.me/{linked_chat.username}>@{linked_chat.title}</a>\n"
                     f"<b>🔗 𝗜𝗻𝘃𝗶𝘁𝗲:</b> {invite}\n\n"
                 )
             j += 1
-        except:
-            text += (
-                f"<b>{j + 1} ➜ </b> {title} [<code>{x}</code>]\n"
-                f"<b>👥 𝗠𝗲𝗺𝗯𝗿𝗼𝘀:</b> <code>{count}</code>\n\n"
-            )
+        except Exception as e:
+            LOGGER(__name__).error(e)
             continue
     if not text:
         await mystic.edit_text(f"➜ 🚫 𝗡ã𝗼 𝗵á 𝗰𝗵𝗮𝘁𝘀 𝗱𝗲 𝘃𝗼𝘇 𝗮𝘁𝗶𝘃𝗼𝘀 𝗲𝗺 {app.mention}.")
@@ -76,7 +68,8 @@ async def activevc(_, message: Message):
 
 @app.on_message(filters.command(["activev", "activevideo"]) & SUDOERS)
 async def activevi_(_, message: Message):
-    mystic = await message.reply_text("➜ 🔄 𝗕𝘂𝘀𝗰𝗮𝗻𝗱𝗼 𝗹𝗶𝘀𝘁𝗮 𝗱𝗲 𝗰𝗵𝗮𝘁𝘀 𝗱𝗲 𝘃𝗶́𝗱𝗲𝗼 𝗮𝘁𝗶𝘃𝗼𝘀...")
+    mystic = await message.reply_text(
+        "➜ 🔄 𝗕𝘂𝘀𝗰𝗮𝗻𝗱𝗼 𝗹𝗶𝘀𝘁𝗮 𝗱𝗲 𝗰𝗵𝗮𝘁𝘀 𝗱𝗲 𝘃𝗶́𝗱𝗲𝗼 𝗮𝘁𝗶𝘃𝗼𝘀...")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
